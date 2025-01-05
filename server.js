f8d6f673379ab9d5ae8f7b900fcebed6482a6819
keshulaptop <keshav@gmail.com>
2025-01-05 16:34:50 +0530

    Updated Content

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
-const express = require('express')
-const app = express()
-const port = 8080
+// server.js
 
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+console.log("Starting the server...");
+
+// Example of adding more detailed output
 app.get('/', (req, res) => {
-	  res.send('Hello World!')
-	res.send('This is my first Demo app Deployment")
-	res.send("for further update Please stay tuned to this page")
-})
+  console.log("Request received on root endpoint");
+  res.send('Hello, World!');
+});
 
 app.listen(port, () => {
-	  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Server is running on port ${port}`);
+});
